3b7e6ee639e22acd079e726fc6c37cc27e93d351
Kauã Peretti <13660986925@edu.udesc.br>
2023-11-07 21:16:10 -0300

    Processamento do formulário de inscrição

--- back-end/processa_form.php
+++ back-end/processa_form.php
@@ -7,7 +7,7 @@ $idade = $_GET['idade'];
     echo '<hr>';
     echo "<p>O usuário $nome tem $idade anos.</p>";
 
-    echo '<a href="http://localhost/udesc/index.html"><button>Home Page</button></a>';
+    echo '<a href="http://localhost/Udesc/projeto/front-end"><button>Home Page</button></a>';
     echo '<button type="button" onclick="javascript:history.back()">Voltar</button>';
-    echo 'button onclick="window.location.href=\'https:\\www.udesc.com.br\'">Vai para Udesc</button>';
+    echo '<button onclick="window.location.href=\'https:\\www.udesc.br\'">Vai para Udesc</button>';
 ?>
